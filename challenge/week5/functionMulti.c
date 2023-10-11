67b7ba243bc2b9163a0039b1b5265533117980ce
bromini <kids2615@naver.com>
2023-10-11 15:06:06 +0900

    week5

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-long mutif(long value); //곱 long 정수형받기
+long multif(long value); //곱 long 정수형받기
 
 void main()
 {
@@ -9,14 +9,14 @@ void main()
 	printf("1부터 5까지의 곱은 %d\n", multif(5)); //1부터 5까지 곱
 }
 
-long mutif(long value)
+long multif(long value)
 {
-	long i ; //i는 1부터 시작
+	long i; //i는 1부터 시작
 	long multi = 0; //합은 0부터 시작
 
-	for (i = 1; i <= value; i= i+1) //i가 value가 될 때 까지 반복 
+	for (i = 1; i <= value; i = i + 1) //i가 value가 될 때 까지 반복 
 	{
-		multi  = multi  + i; // 하고 곱은 곱+ i만큼 증가 
+		multi = multi + i; // 하고 곱은 곱+ i만큼 증가 
 		i++; //i는 1만큼 계속 증가 
 	}
 
